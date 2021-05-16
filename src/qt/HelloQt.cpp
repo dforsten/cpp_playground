338bbd9597322d9e210958b6543f11abcd027063
davidf <dforsten@gmail.com>
2021-05-16 10:26:51 +0200

    Fixed issues reported by clang-tidy and applied clang fmt rules to the Qt example project.

--- src/qt/HelloQt.cpp
+++ src/qt/HelloQt.cpp
@@ -1,22 +1,22 @@
 #include "HelloQt.hpp"
 #include <QHBoxLayout>
-#include <QVBoxLayout>
 #include <QPushButton>
 #include <QLabel>
 #include <QDebug>
+
 
 HelloQt::HelloQt(QWidget *parent) : QWidget(parent)
 {
   // The memory management is not done explicitly but by using the parent
   // relationship built into Qt
-  // The tr calls are not strictly neccesary, but it is all that is needed for
+  // The tr calls are not strictly necessary, but it is all that is needed for
   // future i18n.
-  auto mainLayout = new QVBoxLayout;
-  auto descriptiveLabel = new QLabel(tr("Descriptive"));
-  auto pushButton = new QPushButton(tr("Push me!"));
-  auto grouping = new QHBoxLayout;
-  auto directButton = new QPushButton(tr("Directly!"));
-  auto fatal = new QPushButton(tr("Don't push this button"));
+  auto *mainLayout = new QVBoxLayout;
+  auto *descriptiveLabel = new QLabel(tr("Descriptive"));
+  auto *pushButton = new QPushButton(tr("Push me!"));
+  auto *grouping = new QHBoxLayout;
+  auto *directButton = new QPushButton(tr("Directly!"));
+  auto *fatal = new QPushButton(tr("Don't push this button"));
   // By adding the widgets to the main widget, they get parented and the
   // parenting takes care of freeing the memory.
   // Laying out in layouts makes the GUI automatically resize.
@@ -33,11 +33,11 @@ HelloQt::HelloQt(QWidget *parent) : QWidget(parent)
   connect(pushButton, &QPushButton::clicked, this, &HelloQt::writeToDebug);
   // Lambdas can also be used as can ordinary freestanding functions
   connect(directButton, &QPushButton::clicked, []() { qDebug() << tr("I am in line"); });
-  // Most usefull signals and slots are already built into the framework.
+  // Most useful signals and slots are already built into the framework.
   connect(fatal, &QPushButton::clicked, this, &HelloQt::close);// could just use close
-};
+}
 
-void HelloQt::writeToDebug(void)
+void HelloQt::writeToDebug()
 {
   qDebug() << tr("I don't know what I was supposed to debug here...");
 }
--- src/qt/qtMain.cpp
+++ src/qt/qtMain.cpp
@@ -7,5 +7,5 @@ int main(int argc, char *argv[])
   HelloQt dialog;
   dialog.show();
 
-  return app.exec();// this runs the main event loop and sees to it that cleanup is done
+  return QApplication::exec();// this runs the main event loop and sees to it that cleanup is done
 }
